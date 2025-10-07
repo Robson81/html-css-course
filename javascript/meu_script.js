e12963729d54fe979b5114c4ebf19df45d006cc2
Robson Barbosa <robson.barbosa12@gmail.com>
2025-10-07 08:37:34 -0300

    atualização javaScript

--- javascript/meu_script.js
+++ javascript/meu_script.js
@@ -1,3 +1,9 @@
 
 alert('Olá, seja bem vindo ao curso!')
 document.getElementById('nome').value='Oi'
+//comentário em linha
+/* 
+*********************
+comentário em códigos
+*********************
+*/
